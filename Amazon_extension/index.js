a48fb076c15e92590a902f20b17bf41378063d35
kaimex9 <kaimexalabau@gmail.com>
2025-02-11 19:26:29 +0100

    well well well

--- Amazon_extension/index.js
+++ Amazon_extension/index.js
@@ -7,34 +7,46 @@ function call1() {
     })
 }
 
+function call2() {
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: infoIMGS,
+    })
+}
+
 function openMenu() {
     // Crear el div
     let menuDiv = document.createElement('div');
 
-    // Establecer el estilo del div
-    menuDiv.style.position = 'fixed';   // Para fijarlo en la pantalla
-    menuDiv.style.top = '0';            // Pegado al top de la pantalla
-    menuDiv.style.right = '0';          // Pegado a la derecha
-    menuDiv.style.width = '200px';      // Ancho del div
-    menuDiv.style.height = '100vh';     // Alto del div (100% del alto de la pantalla)
-    menuDiv.style.backgroundColor = '#f0f0f0';  // Color de fondo rojo
-    menuDiv.style.zIndex = '1000';      // Asegura que esté encima de otros elementos
+    menuDiv.style.position = 'fixed';  
+    menuDiv.style.top = '0';            
+    menuDiv.style.right = '0';          
+    menuDiv.style.width = '200px';      
+    menuDiv.style.height = '100vh';     
+    menuDiv.style.backgroundColor = '#f0f0f0';  
+    menuDiv.style.zIndex = '1000';     
 
-    // Agregar el div al cuerpo del documento
+    
     document.body.appendChild(menuDiv);
 
     let title = document.createElement('h2');
     title.textContent = 'Menu de opciones';
     let button1 = document.createElement('button');
     button1.textContent = 'Informacio Imatges';
+    button1.id = "op1";
     let button2 = document.createElement('button');
     button2.textContent = 'Preu mes petit';
+    button2.id = "op2";
     
     menuDiv.appendChild(title);
     menuDiv.appendChild(button1);
     menuDiv.appendChild(button2);
 
-    console.log("Div rojo creado y pegado a la derecha");
+    
+}
+
+function infoIMGS() {
+    console.log("it works")
 }
 
 
